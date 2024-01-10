70fd7b7d17b19160f9cf6713c21f0a7a338cc0e3
kartheeswari <kartheesw2001@gmail.com>
2024-01-10 22:23:59 +0530

    commit

--- Routes/imageupload.route.js
+++ Routes/imageupload.route.js
@@ -28,7 +28,7 @@ const storage = multer.diskStorage({
 
   try {
    
-     const path = `Admin-panel/uploads`;
+     const path = `uploads`;
       console.log(path);
       let upload = await new Image({ ...imagedata, destination: path }).save();
       res.status(201).send({ message: "image upload successfully" });

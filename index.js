8c73a5fcfe792726ad4ade25edf80b1a608a866a
kartheeswari <kartheesw2001@gmail.com>
2024-01-11 01:15:41 +0530

    push

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ app.use(bodyParser.urlencoded({limit:'30mb',extended:true}));
 app.use(bodyParser.json({limit:'30mb',extended:true}));
 app.use(cors());
 const PORT = 5002;
-
+app.use(express.static('uploads'));
 app.get("/", function (request, response) {
   response.send("Welcome to the Mode UI server");
 });
 
 app.use("/api/image", ImageRoutes);
 app.use("/api/clientdata", ClientRoutes);
-app.use("src/Component/uploads",express.static('https://master--enchanting-mochi-38cc25.netlify.app/src/Component/uploads'));
+
 app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));

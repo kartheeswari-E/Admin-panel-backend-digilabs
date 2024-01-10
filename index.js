3d06da4bd6140c6ddbbdd9a8f509499e6193e60b
kartheeswari <kartheesw2001@gmail.com>
2024-01-10 21:16:47 +0530

    commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import db from "./DB/Connect.js";
+import cors from "cors";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import ImageRoutes from "./Routes/imageupload.route.js";
+import ClientRoutes from "./Routes/pro-clientdata.route.js";
+
+dotenv.config();
+db();
+
+const app = express();
+app.use(bodyParser.urlencoded({limit:'30mb',extended:true}));
+app.use(bodyParser.json({limit:'30mb',extended:true}));
+app.use(cors());
+const PORT = 5002;
+
+app.get("/", function (request, response) {
+  response.send("Welcome to the Mode UI server");
+});
+
+app.use("/api/image", ImageRoutes);
+app.use("/api/clientdata", ClientRoutes);
+
+app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
